7cf4d65b18ba050f6ca9195491a037272ecc7509
Dr. Mykola V. Yatsyk <mykola.yatsyk@nure.ua>
2021-11-10 21:16:59 +0200

    Added class Logger

---
+++ src/Logger.h
@@ -0,0 +1,22 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include <stdio.h>
+#include <stdarg.h>
+
+#define Log(TYPE) Logger::getLog(__FILE__, __LINE__, __FUNCTION__, __FUNCSIG__, TYPE)
+
+enum class LOG { CLASS, WM, MESSAGE, ERRORS };
+
+class Logger
+{
+private:
+	Logger();
+	~Logger();
+
+public:
+	static Logger& getLog(const char* file, const long line, const char* function, const char* signature, const LOG type);
+	void print(const char* message, ...);
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <stdlib.h>
 
+#include "Logger.h"
+
 int main(int argc, char* argv[])
 {
+	Log(LOG::MESSAGE).print("Run Application");
+
 	return EXIT_SUCCESS;
 }
